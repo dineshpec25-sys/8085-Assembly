4422d384665c44b87a332731c97ad2911b0f8302
dineshpec25-sys <dineshp.ec25@bitsathy.ac.in>
2026-08-01 12:18:25 +0530

    new and simpler version

---
+++ Assembler/src.2/include.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdint.h>
+
+void parsing(char *input);
+uint8_t solve();
---
+++ Assembler/src.2/main.c
@@ -0,0 +1,44 @@
+#include </home/dinesh-p/8085-Assembly/Assembler/src.2/include.h>
+
+int main()
+{
+    char line[9]; // I am going to exatct line by line from the file and store it in this string
+
+    FILE *fp;
+    FILE *memory;
+    FILE *hex_file;
+    fp = fopen("/home/dinesh-p/8085-Assembly/Assembler/data/test.asm", "r");
+    memory = fopen("/home/dinesh-p/8085-Assembly/Assembler/data/memory.txt", "w");
+    hex_file = fopen("/home/dinesh-p/8085-Assembly/Assembler/data/hexfile.hex", "w");
+
+    if(fp == NULL)
+    {
+        printf("File cannot be opened error !!\n");
+        return 1;
+    }
+    if(memory == NULL)
+    {
+        printf("File cannot be opened error !!\n");
+        return 1;
+    }
+
+    uint16_t LC = 0x0000;
+
+    int n_line=1;
+
+    while(fgets(line, 9, fp) != NULL)
+    {
+        parsing(line);
+
+        uint8_t opcode=solve();
+        fprintf(hex_file, "%02X\n", opcode);
+
+        LC++;
+    }
+
+    fclose(fp);
+    fclose(memory);
+
+    return 0;
+    
+}
